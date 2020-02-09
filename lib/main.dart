260c1caf24c5e0c40b2d5dba4b08211ed012fe90
chinmayshringi4@gmail.com <chinmayshringi4@gmail.com>
2020-02-09 11:45:19 +0530

    UI Started

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_xlider/flutter_xlider.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,20 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Water Level',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Water Level Indicator'),
     );
   }
 }
@@ -44,68 +36,110 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  double rating=0;
+  String dropdownValue = '1';
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return  GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body:
+     Padding(
+        padding: EdgeInsets.all(20),
+        child:Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Text("Enter Volume or Select using scroll"),
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Image.asset("assets/images/water.jpeg",width: MediaQuery.of(context).size.width/2, height: 150,),
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        onChanged: (value){
+                          setState(() {
+                            print(value);
+                          });
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Enter In Liters"
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                        height: 200,
+                        child:FlutterSlider(
+                          rtl: true,
+                          values: [00],
+                          max: 100,
+                          min: 0,
+                          axis: Axis.vertical,
+                          onDragging: (handlerIndex, lowerValue, upperValue) {
+                            rating = lowerValue;
+                            setState(() {});
+                          },
+                        )
+                    )
+                  ],
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+              SizedBox(height: 100,),
+              Row(
+                children: <Widget>[
+                  Text("Select The No. Of containers:",style: TextStyle(fontSize: 24),),
+                ],
+              ),
+            Row(
+              children: <Widget>[
+                SizedBox(width: 100,),
+                DropdownButton<String>(
+                  value: dropdownValue,
+//                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  underline: Container(
+                    height: 2,
+                    color: Colors.black54,
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue=newValue;
+                      print(newValue);
+                    });
+                  },
+                  items: <String>['ALL','1','2','3','4','5']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  })
+                      .toList(),
+                )
+              ],
+            )
           ],
-        ),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
